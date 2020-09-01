a6552f16f644d253ee698c28618dd199dc2e833c
SasounTorossian <46853885+SasounTorossian@users.noreply.github.com>
2020-09-01 21:01:26 +0100

    changed homepage images to webp format to save space. Added backup in home.js.

--- src/home.js
+++ src/home.js
@@ -3,19 +3,22 @@ const content = document.getElementById("content")
 // sections which include image source, image text, header and sections text
 const sections = [
     {
-        "img": "./images/images-home-page/desert-1.jpg", 
+        "img": "./images/images-home-page/desert-1.webp", 
+        "img-backup": "./images/images-home-page/desert-1.jpg", 
         "pImg": "Welcome to Fine Desert Dining",
         "hBody": "Our History",
         "pBody": "le Désert Aride was founded by 2 lost tourists name Pierr Brodeur  and Hans Müller, along with their guide Maalik bin Abubakr. After surviving for multiple days on nothing but the finest insects, oasis water, and camel testicles, the three adventures finally decided to open up a chain of restaurants throughout the world's deserts. Today, their dream of exquisite desert food has come to life, with locations in the Sahara, Namib, Gobi, and other such deserts. We hope you enjoy your stay with us, and remember this experience for the rest of your life."
     },
     {
-        "img": "./images/images-home-page/desert-2.jpg", 
+        "img": "./images/images-home-page/desert-2.webp", 
+        "img-backup": "./images/images-home-page/desert-2.jpg", 
         "pImg": "The Sandiest Cuisine you will Ever Experience",
         "hBody": "The Best Ingredients",
         "pBody": "We use only the finest ingredients from the local area, as well as importing unique ingredients from abroad. This makes our dishes incredibly rare, which is reflected in the pricing. Guests are also welcome to eat any insects or plants they manage to scavenge while seated. Here at le Désert Aride, we strive for the finest food to leave a sandy taste in your mouth."
     },
     {
-        "img": "./images/images-home-page/desert-3.jpg", 
+        "img": "./images/images-home-page/desert-3.webp", 
+        "img-backup": "./images/images-home-page/desert-3.jpg", 
         "pImg": "Come for the Experience, Stay due to Heatstroke",
         "hBody": "Come Get Lost in the Sands of Time",
         "pBody": "Upon wandering into le Désert Aride establishments found only in the finest deserts around the world, you will be greeted by a wandering nomad who will designate a spot for you to sit down. At this point you will be given a menu, and 45 min to decide what delectable meal you would like to seduce your tastebuds with. While there is no formal dress code, we do encourage guests to come prepared for temperatures up to 47°C (117 °F). Such is the experience of le Désert Aride."
@@ -46,6 +49,7 @@ const renderHomePage = () => {
         pimg.setAttribute("data-z-index", "0")
         pimg.setAttribute("data-parallax", "scroll")
         pimg.setAttribute("data-image-src", sec.img)
+        pimg.setAttribute("onerror", `onerror=null; this.src = ${sec["img-backup"]}`)
 
         const ptext = document.createElement("div")
         ptext.classList.add("ptext")
